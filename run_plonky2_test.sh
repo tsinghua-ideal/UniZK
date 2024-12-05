26b841787fef0f11dc4ac825beb5956bac32a7e8
Mo-guan <936255455@qq.com>
2024-12-05 13:21:21 +0800

    update run_plonky2_test.sh

--- run_plonky2_test.sh
+++ run_plonky2_test.sh
@@ -1,10 +1,16 @@
 cp configs/Cargo.toml Cargo.toml
 cargo run -r --example factorial
+rm traces/factorial.bin traces/factorial.txt
 cargo run -r --example fibonacci
+rm traces/fibonacci.bin traces/fibonacci.txt
 cargo run -r --example mvm
+rm traces/mvm.bin traces/mvm.txt
 cargo clean
 cp configs/Cargo.toml.v0.1 Cargo.toml
 cargo run -r --example sha256
+rm traces/sha256.bin traces/sha256.txt
 cargo run -r --example ecdsa
+rm traces/ecdsa.bin traces/ecdsa.txt
 cargo run -r --example img_crop
+rm traces/crop.bin traces/crop.txt
 cargo clean
